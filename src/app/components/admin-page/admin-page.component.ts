46d5cd2194ea022fb4ad5080921f76e6a3ed6215
Jelmer Dijkstra <jelmer.dijkstra@capgemini.com>
2022-04-01 14:40:47 +0200

    admin page updated with buttons to show relevant content

--- src/app/components/admin-page/admin-page.component.ts
+++ src/app/components/admin-page/admin-page.component.ts
@@ -6,11 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-page.component.css']
 })
 export class AdminPageComponent implements OnInit {
-  update: boolean = false;
+  showPerformers: boolean = false;
+  showConcerts: boolean = false;
+  showReviews: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showPerformersClicked(): void {
+    if (this.showConcerts || this.showReviews) {
+      this.showConcerts = false;
+      this.showReviews = false;
+    }
+    this.showPerformers = !this.showPerformers;
+  }
+
+  showConcertsClicked(): void {
+    if (this.showPerformers || this.showReviews) {
+      this.showPerformers = false;
+      this.showReviews = false;
+    }
+    this.showConcerts = !this.showConcerts;
+  }
+
+  showReviewsClicked(): void {
+    if (this.showConcerts || this.showPerformers) {
+      this.showConcerts = false;
+      this.showPerformers = false;
+    }
+    this.showReviews = !this.showReviews;
   }
 
 }
