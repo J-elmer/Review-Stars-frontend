66342586ce1ad2718e7ac68b96ebe1ba22aa2a2c
Jelmer Dijkstra <jelmer.dijkstra@capgemini.com>
2022-04-14 10:04:58 +0200

    updated routing so it works with nginx

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -31,7 +31,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes),
+    [RouterModule.forRoot(routes, { useHash: true })],
     FormsModule
   ],
   exports: [
